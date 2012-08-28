7a2b6fb735c5503bcf9bdd889a67fae0f14afe9a
Paul Vorbach <paul@vorb.de>
2012-08-28 21:39:19 +0200

    Add class Category

--- atomize.js
+++ atomize.js
@@ -30,18 +30,41 @@ var AtomFeed = module.exports.AtomFeed = function (id, title, updated, authors,
 var xmlHeader = '<?xml version="1.0" encoding="utf-8"?>';
 var xmlns = 'xmlns="http://www.w3.org/2005/Atom"';
 
-AtomFeed.prototype.toString() = function(root, space) {
+AtomFeed.prototype.toString() = function (root, space) {
   if (typeof root == 'undefined' || root === null)
     root = 'feed';
   if (typeof space == 'undefined' || space === null)
     space = '  ';
 
-  var xml = xmlHeader + '\n' + 
+  var xml = xmlHeader + '\n' +
 }
 
 var Entry = module.exports.Entry = function () {};
 
-var Person = module.exports.Person = function(name, uri, email) {
+var Category = module.exports.Category = function (term, scheme, label) {
+
+  // required field
+  this.term = term;
+
+  // optional fields
+  this.scheme = scheme;
+  this.label = label;
+};
+
+Category.prototype.toString = function (space, level) {
+  if (typeof space == 'undefined' || space == null)
+    space = '  ';
+  if (!level)
+    level = 0;
+
+  var xml = repeatString(space, level)+'<category term="'+this.term+'"';
+  if (this.scheme)
+    xml += ' scheme="'+this.scheme+'"';
+  if (this.label)
+    xml += ' label="'+this.label+'"';
+};
+
+var Person = module.exports.Person = function (name, uri, email) {
 
   // required field
   this.name = name;
